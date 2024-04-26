0d562c94486dec05fea6857aeadb6e5a98f3050e
Jonathas Santos <jonathaspe@hotmail.com>
2024-04-26 19:01:43 +0100

    Controles de fluxo com BDD

--- js/app.js
+++ js/app.js
@@ -14,7 +14,9 @@ var userName = 'Jonathas Santos'
 
 document.getElementById('user').innerHTML = userName
 
-/* Operadores matemáticos 
+// Operadores matemáticos, operadores de comparação e funções
+
+/* //Operadores matemáticos //
 
 var n1 = 5
 var n2 = 2.5
@@ -32,7 +34,7 @@ var total = n1 / n2
 console.log(total)
 */
 
-/* Operadores de comparação 
+/* //Operadores de comparação //
 
 var v1 = 5
 var v2 = 5
@@ -50,7 +52,7 @@ var resultado = v1 === v2
 console.log(resultado)
 */
 
-/* Funções
+/* //Funções//
 
 function soma(n1, n2) {
     console.log( n1 + n2)
@@ -62,7 +64,7 @@ function boasVindas(nome) {
     alert(nome + ', seja bem-vindo(a)')
 }
 
-boasVindas('Jonathas Santos')*/
+boasVindas('Jonathas Santos')
 
 function soma(n1, n2) {
     return n1 + n2
@@ -70,3 +72,69 @@ function soma(n1, n2) {
 
 var resultado = soma(5, 10)
 console.log(resultado)
+*/
+
+
+// Cotrole de fluxos //
+
+// Como um cliente correntis de um banco
+// Quero sacar dinheiro em caixas eletrônicos
+// Para poder comprar em lugares que não aceitam o cartão de débito ou crédito
+
+
+// Cenário 1: Saque com sucesso
+// Dado que meu saldo é de 1000 reais
+// Quando eu faço um saque de 500 reais
+// Então o valor é debitado do meu saldo
+
+var saldo = 1000
+
+function saque(valor) {
+    saldo = saldo - valor
+}
+
+saque(500)
+console.log(saldo)
+
+// Cenário 2: Saque com valor superior ao saldo em conta
+// Dado que o meu saldo é de 1000 reais
+// Quando eu faço um saque de 1001 reais
+// Então não de permitir o saque
+// E deve mostrar uma mensagem de alerta informando que o valor é superior ao saldo
+
+var saldo = 1000
+
+function saque(valor) {
+    if(valor > saldo) {
+        console.log('Valor do saque superior ao saldo')
+    }else{
+        saldo = saldo - valor
+    }
+    
+}
+
+saque(1001)
+console.log(saldo)
+
+// Cenário 3: saque com o valor máximo
+// Dado que o meu saldo é de 1000 reais
+// E que o valor máximo por operação é de 700 reais
+// Quando eu faço um saque de 701 reais
+// Então não de permitir o saque
+// E deve mostrar uma mensagem de alerta informando que o valor é superior ao máximo permitido por operação
+
+var saldo = 1000
+
+function saque(valor) {
+    if(valor > saldo) {
+        console.log('Valor do saque superior ao saldo')
+    }else if (valor > 700){
+        console.log('Valor do saque superior ao máximo permitido por operação')
+    }else{
+        saldo = saldo - valor
+    }
+    
+}
+
+saque(701)
+console.log(saldo)
